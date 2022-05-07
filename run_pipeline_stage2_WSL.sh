6991e339ac5e5522c0f1fcfba3119a4621ca746b
CarlM <carl.moore@image-metrics.com>
2022-05-07 11:14:42 +0100

    fixes

--- run_pipeline_stage2_WSL.sh
+++ run_pipeline_stage2_WSL.sh
@@ -1,38 +1,33 @@
 #!/usr/bin/bash
+#example usage: sh run_pipeline_stage2_WSL.sh example_dance /mnt/c/temp/MTC-Data-Win /mnt/c/Users/CarlM/Downloads/openpose-1.7.0-binaries-win64-gpu-python3.7-flir-3d_recommended/openpose C:/temp/MTC-Data-Win/example_dance
 
 set -e
-# input param 
+# input param
 # $1: sequence name
 # $2: path to the parent folder of the data that you want to use.
 # Data would be structured as $2/example_dance/example_dance.avi
-# $3: whether the video is upper body only (false by default, enable by -f)
+# $3: path to the openpose windows binaries root i.e. the path to the folder that contains the bin and models dirs. should be in Linux style, e.g. /mnt/c/temp...
 seqName=$1
-dataDir=$2
-openPoseDir=$3
+wslDataDir=$2
+openPosePath=$3
+windowsSequencePath=$4
 
-if [ -z "$dataDir" ]
+if [ -z "$wslDataDir" ]
 then
-  dataDir=./data/
+  wslDataDir=./data/
 fi
 
-if [ -z "$openposeDir" ]
-then
-  openposeDir=../openpose/
-fi
-
-echo "Using dataDir: " $dataDir
-
-# Git clone openpose to ../openpose and compile with cmake
-openposeDir=../openpose/
+echo "Using wslDataDir: " $wslDataDir
 
 # convert to absolute path
 MTCDir=$(readlink -f .)
-dataDir=$(readlink -f $dataDir)
-openposeDir=$(readlink -f $openposeDir)
+wslDataDir=$(readlink -f $wslDataDir)
 
+cd $wslDataDir/$seqName
 # run OpenPose on image frames
 if [ ! -d openpose_result ]; then
-	mkdir openpose_result
-	cd $openposeDir
-	./build/examples/openpose/openpose.bin --face --hand --image_dir $dataDir/$seqName/raw_image --write_json $dataDir/$seqName/openpose_result --render_pose 0 --display 0 -model_pose BODY_25 --number_people_max 1
+        echo "Running openpose"
+        mkdir $wslDataDir/$seqName/openpose_result
+        cd $openPosePath
+        $openPosePath/bin/OpenPoseDemo.exe --face --hand --image_dir $windowsSequencePath/raw_image --write_json $windowsSequencePath/openpose_result --render_pose 0 --display 0 -model_pose BODY_25 --number_people_max 1
 fi
